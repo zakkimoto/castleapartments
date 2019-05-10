a173db44880e36ff52d90d957e9507d7a5da9947
Arna Olafsdottir <arnao17@ru.is>
2019-05-10 14:15:29 +0000

    search bar but not fully functioning

---
+++ static/js/properties.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#search-btn').on('click', function(e){
+        e.preventDefault();
+        var searchText = $('#search-box').val();
+        $.ajax({
+            url: '/properties/?search_filter=' + searchText,
+            type: 'GET',
+            success: function(resp) {
+                var newHtml = resp.data.map(d => {
+                    return `<div class="well property">
+                                <a href="/properties/${d.id}">
+                                    <img class="property-img" src="${d.firstImage}"/>
+                                    <h4>${d.name}</h4>
+                                    <p>${d.description}</p>
+                                </a>
+                            </div>`
+                });
+                $('.properties').html(newHtml.join(''));
+                $('#search-box').val('');
+            },
+            error: function (xhr, status, error) {
+                // TODO: Show toastr
+                console.error(error)
+            }
+        });
+    });
+});
