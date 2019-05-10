eba179a66b36ae5d28d2e8aad315ecc2bf813411
Arna Olafsdottir <arnao17@ru.is>
2019-05-10 11:18:26 +0000

    able to add and delete property

--- property/urls.py
+++ property/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="property-index"),
     path('<int:id>', views.get_property_by_id, name="property_details"),
+    path('create_property', views.create_property, name="create_property"),
+    path('delete_candy/<int:id>', views.delete_property, name="delete_property"),
 ]
--- property/views.py
+++ property/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from property.models import Property
+from django.shortcuts import render, get_object_or_404, redirect
+
+from property.forms.property_form import PropertyCreateForm
+from property.models import Property, PropertyImage
 
 
 def index(request):
@@ -10,3 +12,23 @@ def get_property_by_id(request, id):
     return render(request, 'property/property_details.html', {
         'property': get_object_or_404(Property, pk=id)
     })
+
+def create_property(request):
+    if request.method == 'POST':
+        form = PropertyCreateForm(data=request.POST)
+        if form.is_valid():
+            property = form.save()
+            property_image = PropertyImage(image=request.POST['image'], property=property)
+            property_image.save()
+            return redirect('property-index')
+
+    else:
+        form = PropertyCreateForm()
+    return render(request, 'property/create_property.html', {
+        'form': form
+    })
+
+def delete_property(request, id):
+    property = get_object_or_404(Property, pk=id)
+    property.delete()
+    return redirect('property-index')
