1a801a4ed334021bb9ee2e7520412e64e850412a
sigridurg17 <42962130+sigridurg17@users.noreply.github.com>
2019-05-14 12:44:15 +0000

    Search completed

--- home/views.py
+++ home/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from property.models import Property
 
 # Create your views here.
 
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        properties = [{
+            'id': x.id,
+            'streetname': x.streetname,
+            'description': x.description,
+            'firstImage': x.propertyimage_set.first().image,
+            'country': x.country,
+            'size': x.size,
+        } for x in Property.objects.filter(streetname__contains=search_filter)]
+        return JsonResponse({'data': properties})
+    context = {'properties': Property.objects.all().order_by('streetname')}
+    return render(request, 'home/index.html', context)
+
---
+++ static/js/home.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+    console.log('HOME clicked search btn')
+    $('.search-btn').on('click', function(e) {
+        e.preventDefault();
+        var searchText = $('.search-box').val();
+        $.ajax({
+            url: '/properties?search_filter=' + searchText,
+            type: 'GET',
+            success: function(resp){
+                var newHTML = resp.data.map(d => {
+                    return `<div class="well property">
+                                <a href="/properties/${d.id}">
+                                    <img class="property-img" src="${d.firstImage}"/>
+                                    <h4>${d.streetname}</h4>
+                                    <p>${d.description}</p>
+                                </a>
+                               </div>`
+                });
+                $('.properties').html(newHTML.join(''));
+                $('.search-box').val('');
+
+            },
+            error: function(xhr, status, error){
+                console.error(error)
+            }
+        })
+    });
+
+
+
+});
