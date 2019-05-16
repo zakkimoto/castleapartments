1da3e316557d238a16be67122a8176753da37dc5
sarabry17 <sarabry17@ru.is>
2019-05-16 22:09:23 +0000

    styling

--- seller/models.py
+++ seller/models.py
@@ -1,7 +1,7 @@
 
 from django.db import models
 
-# Create your models here.
+# Create eayour models here.
 class Seller(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
